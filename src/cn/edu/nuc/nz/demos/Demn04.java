0c19d93351916b5a2c0a8386f06a60a0433a5cb6
lzm <nmlzm0423>
2020-04-07 13:18:14 +0800

    Demo4更新了

---
+++ src/cn/edu/nuc/nz/demos/Demn04.java
@@ -0,0 +1,15 @@
+/*
+ *   中北大学软件学院(c) 版权所有 2020-2021.
+ */
+package cn.edu.nuc.nz.demos;
+
+/**
+ * 
+ *  @author lizhimin
+ *  @version 1.0
+ */
+public class Demn04 {
+	public static void main(String[] args) {
+		System.out.println("Demo4更新了");
+	}
+}
