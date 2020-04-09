5622b0cbdf6ab8db2733378dbf94823ff015ce1c
lzm <nmlzm0423>
2020-04-09 12:36:28 +0800

    静态成员方法的用途，静态代码块的用途

--- src/cn/edu/nuc/nz/demos/Demo9.java
+++ src/cn/edu/nuc/nz/demos/Demo9.java
@@ -11,10 +11,12 @@ import java.util.Arrays;
  *   		2.静态方法不能使用非静态的成员变量和非静态的成员方法，但是外界传数据不影响
  *   		3静态方法常用于工具类  （Arrays 的 sort()  binarySearch() toString）	 
  *        工具类的分装
+ *   static修饰代码块  静态代码块  里面声明的变量是局部变量   主要用于一些配置文件的加载
  * 
  *  @author lizhimin
  *  @version 1.0
  */
+
 class MyArray {
 	/* 工具类中大多是静态方法，为了方便 */
 	/**
